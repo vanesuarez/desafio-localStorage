3b03920c05310b00c85eec7a558baa826721f0ae
Vanesa Suarez <vanesuarez2301@gmail.com>
2023-08-22 20:59:24 -0300

    simplificando codigo

--- js/data.js
+++ js/data.js
@@ -1,10 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Obtener el dato del localStorage
-    const datoGuardado = localStorage.getItem('datos'); // llamar el nombre, no el contenido
-    // Comprobar si el dato existe en el localStorage
-    if (datoGuardado) {
-        // Mostrar el dato en la página
-        const dataSpan = document.getElementById('data');
-        dataSpan.textContent = datoGuardado;
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtener el dato del localStorage
+  const datoGuardado = localStorage.getItem("datos"); // llamar el nombre
+  // Mostrar el dato en la página
+  const dataSpan = document.getElementById("data");
+  dataSpan.textContent = datoGuardado;
 });
